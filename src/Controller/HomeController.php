10947fedc286e471459f0e13c2b7f541174c1893
Facundo <facundosnake@gmail.com>
2024-09-20 14:37:30 -0300

    Mostrar Alumnos en Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Alumno;
+use App\Form\AlumnoType;
+use App\Repository\AlumnoRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(AlumnoRepository $AlumnoRepository): Response
     {
+        $alumnos = $AlumnoRepository->findALL();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'alumnos' => $alumnos,
         ]);
     }
 }
